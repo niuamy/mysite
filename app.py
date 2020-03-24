44fd078959af4127dd98a2b33185c47a883aba87
niuamy <amyyniu@gmail.com>
2020-03-23 22:21:50 -0400

    recruiter and friend html/css

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 from flask import Flask, render_template, request
 
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-
 @app.route('/hello', methods=["POST"])
 def hello():
     name = request.form.get("name")
     return render_template("hello.html", name=name)
 
-@app.route('/welcome', methods=["POST"])
-def welcome():
-    return render_template("welcome.html")
+@app.route('/welcomeRecruiter', methods=["POST"])
+def welcomeRecruiter():
+    return render_template("welcomeRecruiter.html")
+
+@app.route('/welcomeFriend', methods=["POST"])
+def welcomeFriend():
+    return render_template("welcomeFriend.html")
+
+@app.route('/friend', methods=["POST"])
+def friend():
+    return render_template("friend.html")
